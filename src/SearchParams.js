748ba94c972d56a26bbd47e7fc81f5d40f98e7fc
Jakob <jakobmsgrant@gmail.com>
2020-05-07 11:05:15 -0700

    end hooks, begin env and strict mode

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  //after SearchParams() has fully ran, then useEffects will follow
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
+
   return (
     <div className="search-params">
       <form>
@@ -21,7 +33,6 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
-
         <button>Submit</button>
       </form>
     </div>
