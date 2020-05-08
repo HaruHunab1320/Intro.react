fb84077cbd04e8b022908bcc64cd5b80220ef3a8
Jakob <jakobmsgrant@gmail.com>
2020-05-07 17:43:56 -0700

    Turned Details.js into class component

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,43 @@
 import React from "react";
+import pet from '@frontendmasters/pet';
 
-const Details = (props) => {
-  return (
-    //This is a debugging technique to get a full list of the info that the router is pulling in, or use react dev tool in inspector
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+//class components must have a render method
+//You cannot use hooks in class components
+render() {
+//component did mount runs once when things are created.
+this.componentDidMount () {
+    constructor(props) {
+        //super props says call the constructor on my parent class called React.Component
+        super(props);
+        //this.state is self contained within the class. no other component can modify its state.
+        //this.state is instantiating the initial state
+        this.state = {
+            loading: true
+        };
+    }
+    //this.props is immutable info coming from the parent
+    pet.animal(this.props.id)
+    .then(({ animal }) => {
+        //this.setState will update the above this.state loading: true
+        //This is considered a "shallow merge", anything that collides with be overwritten, everything else is added. deeply nested items will be overwritten as it only does the top level. 
+        this.setState({
+            name: animal.name,
+            animal: animal.type,
+            location: `${animal.contact.address.city}, 
+            ${animal.contact.address.state}`,
+            description: animal.description,
+            media: animal.photos,
+            breed: animal.breeds.primary,
+            loading: false
+        })
+    })
+}
+render() {
+
+}
+}
+
+}
 
 export default Details;
