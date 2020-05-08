7b685e0309d5949982f9ce98d48e02d6d6f35bf6
Jakob <jakobmsgrant@gmail.com>
2020-05-08 10:24:00 -0700

    persisting State with Context hooks

--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 //Use detructuring to pull out the specific properties to avoid writing something like props.name, props,animal, props.breed
 export default function Pet({ name, animal, breed, media, location, id }) {
@@ -7,8 +8,12 @@ export default function Pet({ name, animal, breed, media, location, id }) {
     hero = media[0].small;
   }
 
+  //with a hook like [ foo, setFoo] = useState('blah), if you only wanted the "setFoo" value, you can use a _ as a place holder and ignore the lint checker with a comment
+  //eg. [ _, setFoo ] = useState('blah') //eslint-disable-line no-unused-cars
+
   return (
-    <a href={`/details/${id}`} className="pet">
+    //changed this to <Link> because <a> tag destroys the DOM and regenerates it, where link redirects without detroying the DOM
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -16,6 +21,6 @@ export default function Pet({ name, animal, breed, media, location, id }) {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 }
