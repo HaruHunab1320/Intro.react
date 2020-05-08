a4a9f56f57ac83bacc152d5005612a206af7811e
Jakob <jakobmsgrant@gmail.com>
2020-05-08 11:26:36 -0700

    displaying modal

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
+import Modal from "./Modal";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
@@ -7,7 +9,7 @@ import ThemeContext from "./ThemeContext";
 //Details is a class so we cannot use any hooks. To use context with classes we need to build a react component within <ThemeContext.Consumer
 class Details extends React.Component {
   //this takes place of the constructor below this. We had to access the babel config to allow this
-  state = { loading: true };
+  state = { loading: true, showModal: false };
   //   //class components must have a render method
   //   //You cannot use hooks in class components
   //   //component did mount runs once when things are created.
@@ -26,6 +28,7 @@ class Details extends React.Component {
       //this.setState will update the above this.state loading: true
       //This is considered a "shallow merge", anything that collides with be overwritten, everything else is added. deeply nested items will be overwritten as it only does the top level.
       this.setState({
+        url: animal.url,
         name: animal.name,
         animal: animal.type,
         location: `${animal.contact.address.city}, 
@@ -37,11 +40,21 @@ class Details extends React.Component {
       });
     }, console.error);
   }
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  adopt = () => navigate(this.state.url);
   render() {
     if (this.state.loading) {
       return <h1>loading...</h1>;
     }
-    const { animal, breed, location, description, name, media } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+      showModal,
+    } = this.state;
 
     //Any function that returns markup is a react component. so in the example below we are technically creating a smaller react component within the ThemeContext.Consumer
     return (
@@ -51,13 +64,27 @@ class Details extends React.Component {
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
           <ThemeContext.Consumer>
-            {(themeHook) => (
-              <button style={{ backgroundColor: themeHook[0] }}>
+            {([theme]) => (
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
                 Adopt {name}
               </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No, Im a monster</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
