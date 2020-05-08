8968e1e0178675d704a038cf92a4b852280de177
Jakob <jakobmsgrant@gmail.com>
2020-05-07 18:31:13 -0700

    configured babel for parcel

--- src/Details.js
+++ src/Details.js
@@ -1,43 +1,55 @@
 import React from "react";
-import pet from '@frontendmasters/pet';
+import pet from "@frontendmasters/pet";
 
 class Details extends React.Component {
-//class components must have a render method
-//You cannot use hooks in class components
-render() {
-//component did mount runs once when things are created.
-this.componentDidMount () {
-    constructor(props) {
-        //super props says call the constructor on my parent class called React.Component
-        super(props);
-        //this.state is self contained within the class. no other component can modify its state.
-        //this.state is instantiating the initial state
-        this.state = {
-            loading: true
-        };
-    }
+  //this takes place of the constructor below this. We had to access the babel config to allow this
+  state = { loading: true };
+  //   //class components must have a render method
+  //   //You cannot use hooks in class components
+  //   //component did mount runs once when things are created.
+  //   constructor(props) {
+  //     //super props says call the constructor on my parent class called React.Component
+  //     super(props);
+  //     //this.state is self contained within the class. no other component can modify its state.
+  //     //this.state is instantiating the initial state
+  //     this.state = {
+  //       loading: true,
+  //     };
+  //   }
+  componentDidMount() {
     //this.props is immutable info coming from the parent
-    pet.animal(this.props.id)
-    .then(({ animal }) => {
-        //this.setState will update the above this.state loading: true
-        //This is considered a "shallow merge", anything that collides with be overwritten, everything else is added. deeply nested items will be overwritten as it only does the top level. 
-        this.setState({
-            name: animal.name,
-            animal: animal.type,
-            location: `${animal.contact.address.city}, 
+    pet.animal(this.props.id).then(({ animal }) => {
+      //this.setState will update the above this.state loading: true
+      //This is considered a "shallow merge", anything that collides with be overwritten, everything else is added. deeply nested items will be overwritten as it only does the top level.
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, 
             ${animal.contact.address.state}`,
-            description: animal.description,
-            media: animal.photos,
-            breed: animal.breeds.primary,
-            loading: false
-        })
-    })
-}
-render() {
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+    const { animal, breed, location, description, name } = this.state;
 
-}
-}
-
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Details;
